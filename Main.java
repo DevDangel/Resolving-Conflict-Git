affc2a4ede90a839f87e3300760398e699bc214a
DevDangel <angeldavidh18@gmail.com>
2025-09-02 21:54:31 -0500

    se creo la funcion recursiva fibonacci

--- Main.java
+++ Main.java
@@ -1,3 +1,11 @@
 public class Main{
     // all code here 
+
+    // need method fibonacci
+    public static int fibonacci(int n){
+        if ( n <= 1 ) return n;
+        else{
+            return fibonacci(n - 1) + fibonacci(n - 2);
+        }
+    }
 }

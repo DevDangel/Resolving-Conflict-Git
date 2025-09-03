bb7032444ac0a458e7326dee68b1d93511a4f9d2
DevDangel <angeldavidh18@gmail.com>
2025-09-02 22:48:18 -0500

    method mltp

--- Main.java
+++ Main.java
@@ -17,4 +17,12 @@ public class Main{
             return fibonacci(n - 1) + fibonacci(n - 2);
         }
     }
+
+    public static void mltp(){
+        for (int i = 1; i <= 10; i++){
+            for (int j = 1; j <= 10; j++){
+                System.out.print(i+" x " + j + " = " + (i * j) + "\t");
+            }
+        }
+    }
 }
